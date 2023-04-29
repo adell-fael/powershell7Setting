85d90399fcc9ded2c9388e0912a30ddb794eaa48
Adell Fael <new19.adel@gmail.com>
2023-04-29 15:37:21 +0300

    1st commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,11 @@
+
+
+
+Import-Module -Name Terminal-Icons
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+
+
+New-Alias -Name pn -Value pnpm
