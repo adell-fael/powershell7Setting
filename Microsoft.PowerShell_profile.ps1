48dd90879103e0db913f525764f173ad60fd832a
Adell Fael <new19.adel@gmail.com>
2023-07-12 16:24:46 +0300

    updating the logic for detecting .nvmrc file

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -20,11 +20,31 @@ New-Alias -Name pn -Value pnpm
 $nodeVersion = Get-Content -Path ".nvmrc" -ErrorAction SilentlyContinue
 
 if ($nodeVersion) {
-		nvm use $nodeVersion
-		Write-Output "Node.js version $nodeVersion has been activated."
+	nvm use $nodeVersion
+	Write-Output "Node.js version $nodeVersion has been activated."
 } 
 else {
-		# Write-Output "No .nvmrc file found in the current directory."
-		nvm use latest | Out-Null
-}
+	$latestVersion = (nvm list available | Select-String -Pattern '\|\s+([^\s]+)\s+\|' | ForEach-Object { $_.Matches.Groups[1].Value } | Where-Object { $_ -notmatch 'CURRENT' } | Sort-Object -Descending | Select-Object -First 1).Trim()
+	$currentVersion = nvm current
 
+	if ($latestVersion) {
+		if ($currentVersion) {
+			if ($currentVersion -replace "v", "" -eq $latestVersion) {
+				Write-Output "You are already using the latest Node.js version ($currentVersion)."
+			}
+			else {
+				Write-Output "Current Node.js version: $currentVersion"
+				Write-Output "Latest Node.js version: $latestVersion"
+				Write-Output "To install and use the latest version, run:"
+				Write-Output "nvm install $latestVersion"
+				Write-Output "nvm use $latestVersion"
+			}
+		}
+		else {
+			Write-Output "Unable to determine the current Node.js version."
+		}
+	}
+	else {
+		Write-Output "No available Node.js versions found."
+	}
+}
